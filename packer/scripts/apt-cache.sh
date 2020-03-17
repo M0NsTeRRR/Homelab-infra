252c634c0609879a181fbb75ee9afb8e93f085f7
Ludovic Ortega <mastership@hotmail.fr>
2020-03-17 16:56:44 +0100

    Update packer configuration

--- packer/scripts/apt-cache.sh
+++ packer/scripts/apt-cache.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+cat <<EOT >> /etc/apt/detect-http-proxy.sh
+#!/bin/bash
+proxy=192.168.10.51
+
+nc -zw1 $proxy 3128 && echo http://$proxy:3128/ || echo DIRECT
+EOT
+
+chmod +x /etc/apt/detect-http-proxy.sh
+
 cat <<EOT >> /etc/apt/apt.conf.d/01proxy
-Acquire::HTTP::Proxy "http://192.168.10.51:3142";
+Acquire::HTTP::ProxyAutoDetect "/etc/apt/detect-http-proxy.sh";
 Acquire::HTTPS::Proxy "DIRECT";
 EOT
 
