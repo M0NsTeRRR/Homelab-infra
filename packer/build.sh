47d563ae5a65b87f042ccff1183db5cd393480f9
Ludovic Ortega <mastership@hotmail.fr>
2020-03-17 01:56:54 +0100

    Update packer (Migrate cluster to ESXI)

--- packer/build.sh
+++ packer/build.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
-export PACKER_LOG_PATH=log
 export PACKER_CACHE_DIR=packer_cache
 
 set -e
 
-proxmox_url='https://192.168.40.11:8006/api2/json'
-proxmox_username='root@pam'
+vcenter_server='192.168.40.10'
+username='administrator@vsphere.local'
 
 read -p 'host_ip: ' host_ip
 read -p 'distribution: ' distribution
 read -p 'version: ' version
-printf "proxmox_url: $proxmox_url\n"
-read -sp 'proxmox_password: ' proxmox_password
-printf "\n"
-read -p 'proxmox_node: ' proxmox_node
+read -sp 'password: ' password
 
 packer build \
   -var "host_ip=$host_ip" \
   -var "distribution=$distribution" \
   -var "version=$version" \
-  -var "proxmox_url=$proxmox_url" \
-  -var "proxmox_username=$proxmox_username" \
-  -var "proxmox_password=$proxmox_password" \
-  -var "proxmox_node=$proxmox_node" \
-  templates/"$distribution"-proxmox.json | tee log/packer_build.log
+  -var "vcenter_server=$vcenter_server" \
+  -var "username=$username" \
+  -var "password=$password" \
+  -timestamp-ui \
+  templates/"$distribution".json
--- packer/validate.sh
+++ packer/validate.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
-export PACKER_LOG_PATH=log
 export PACKER_CACHE_DIR=packer_cache
 
 set -e
 
 for distribution in "debian" "ubuntu"; do
   packer validate \
-    -var "host_ip=192.168.0.2" \
-    -var "distribution=$distribution" \
-    -var "version=18.04.3" \
-    -var "proxmox_url=https://test.fr:8006/api2/json" \
-    -var "proxmox_username=test" \
-    -var "proxmox_password=test" \
-    -var "proxmox_node=test" \
-    templates/"$distribution"-proxmox.json | tee log/packer_validate.log
+	-var "host_ip=192.168.0.1" \
+	-var "distribution=$distribution" \
+	-var "version=10" \
+	-var "vcenter_server=192.168.0.1" \
+	-var "username=administrator@vsphere.local" \
+	-var "password=test" \
+	-timestamp-ui \
+	templates/"$distribution".json
 done
