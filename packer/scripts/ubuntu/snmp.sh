6e4c722913ded9bd1e3f35d578d341ce5ccf94b6
Ludovic Ortega <mastership@hotmail.fr>
2020-06-11 16:48:16 +0200

    [Fix] Restart snmpd after configuration update

--- packer/scripts/ubuntu/snmp.sh
+++ packer/scripts/ubuntu/snmp.sh
@@ -6,3 +6,5 @@ sed -i '/agentaddress  127.0.0.1,\[::1\]/c\agentaddress 0.0.0.0,::' /etc/snmp/sn
 
 sed -i '/rocommunity  public default -V systemonly/c\rocommunity unicornafk default' /etc/snmp/snmpd.conf
 sed -i '/rocommunity6 public default -V systemonly/c\rocommunity6 unicornafk default' /etc/snmp/snmpd.conf
+
+systemctl restart snmpd
