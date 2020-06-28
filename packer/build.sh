ac9d8b8fb3f90915d1dece8484911f2841780c8d
Ludovic Ortega <mastership@hotmail.fr>
2020-06-28 04:05:23 +0200

    [Update] Refactor template and remove unused validate.sh

--- packer/build.sh
+++ packer/build.sh
@@ -4,12 +4,12 @@ export PACKER_CACHE_DIR=packer_cache
 set -e
 
 vcenter_server='vsphere.unicornafk.fr'
-username='administrator@vsphere.unicornafk.fr'
+vcenter_username='administrator@vsphere.unicornafk.fr'
 
 read -p 'host_ip: ' host_ip
 read -p 'distribution: ' distribution
 read -p 'version: ' version
-read -sp 'password: ' password
+read -sp 'vcenter_password: ' vcenter_password
 printf "\n"
 
 packer build \
@@ -17,7 +17,7 @@ packer build \
   -var "distribution=$distribution" \
   -var "version=$version" \
   -var "vcenter_server=$vcenter_server" \
-  -var "username=$username" \
-  -var "password=$password" \
+  -var "vcenter_username=$vcenter_username" \
+  -var "vcenter_password=$vcenter_password" \
   -timestamp-ui \
   templates/"$distribution".json
